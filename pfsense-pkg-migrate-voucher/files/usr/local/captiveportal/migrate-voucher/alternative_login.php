b6cdf9d8c6aba7d4cbfa73a80a0f7b70fbee3c7b
Max Ammann <maximilian.ammann@googlemail.com>
2018-03-07 14:44:08 +0100

    Show message

--- pfsense-pkg-migrate-voucher/files/usr/local/captiveportal/migrate-voucher/alternative_login.php
+++ pfsense-pkg-migrate-voucher/files/usr/local/captiveportal/migrate-voucher/alternative_login.php
@@ -6,13 +6,19 @@ if ($_SERVER['REQUEST_METHOD'] != 'GET') {
 }
 
 http_response_code(200);
-?>
 
-<script type="text/javascript">
-    window.onload = function () {
-        document.forms["proxy-form"].submit();
-    }
-</script>
+if (!isset($_GET['message'])) {
+    ?>
+    <script type="text/javascript">
+        window.onload = function () {
+            document.forms["proxy-form"].submit();
+        }
+    </script>
+    <?php
+}
+
+echo($_GET['message'])
+?>
 
 <form name='proxy-form' method="post" action="<?php echo($_GET['action']) ?>">
     <input name="auth_user" type="hidden" value="<?php echo($_GET['auth_user']) ?>">
@@ -21,4 +27,6 @@ http_response_code(200);
     <input name="redirurl" type="hidden" value="<?php echo($_GET['redirurl']) ?>">
     <input name="zone" type="hidden" value="<?php echo($_GET['zone']) ?>">
     <input name="accept" type="submit" value="Continue">
+
+    <input name="message" type="hidden" value="<?php echo($_GET['message']) ?>">
 </form>
