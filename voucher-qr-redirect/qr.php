01d7bb488c74f949892471984ffec34789f1c9e3
Max Ammann <maximilian.ammann@googlemail.com>
2018-05-23 16:37:17 +0200

    Add action for qr code redirects

--- voucher-qr-redirect/qr.php
+++ voucher-qr-redirect/qr.php
@@ -20,6 +20,7 @@ http_response_code(200);
     <input name="auth_voucher" type="hidden" value="<?php echo($_GET['voucher']) ?>">
     <input name="redirurl" type="hidden" value="https://www.integreat-app.de/">
     <input name="zone" type="hidden" value="tatdf">
+    <input name="action" type="hidden" value="<?php echo($_GET['action']) ?>">
 
     <!--Captiveportal needs this input and javascript does not submit if type=submit-->
     <input name="accept" type="hidden" value="dummy">
