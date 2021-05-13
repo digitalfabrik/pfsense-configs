8428731e410d9381044772c886a70f8883f4e10a
Maximilian Ammann <max@maxammann.org>
2021-05-13 17:09:30 +0200

    Add corona captive portal

--- scripts/backup-configs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mkdir -p config-backups
-
-for domain in "$@"
-do
-	ssh backup@$domain cat /cf/conf/config.xml | gpg --encrypt -r max@maxammann.org -r seeberg@integreat-app.de -o ~/config-backups/$domain-$(date +"%Y%m%d-%H%M%S")-config.xml.gpg
-done
-
